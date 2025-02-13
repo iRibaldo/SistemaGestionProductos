ed2f485eeec30ae77eb6b83519ea0f5631d87d2d
Ribaldo  Nantip <ribaldo.nantipbarre3a@gmail.com>
2025-02-13 13:28:27 -0500

    Primera Subida

---
+++ src/main/java/ec/edu/espoch/sistemagestionproductos/SistemaGestionProductos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sistemagestionproductos;
+
+/**
+ *
+ * @author SO-LAB1-PC27
+ */
+public class SistemaGestionProductos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
